6d7b24ddd3fc8ae378f6e504cec5ed8c29886595
BrendaGathogo <gathogobrenda20@gmail.com>
2023-03-10 14:53:20 +0000

    django commit

--- AlkidDjangoProject1/urls.py
+++ AlkidDjangoProject1/urls.py
@@ -15,6 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('home/', views.homepage, name='home'),
+    path('about/', views.aboutpage, name='about'),
+    path('services/', views.servicespage, name='services'),
+    path('blog/', views.blogpage, name='blog'),
+    path('contact/', views.contactpage, name='contact'),
+    path('product/', views.productpage, name='product')
 ]
